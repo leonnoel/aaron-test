69c2d0ff6a279f2973f7795e30dc885e22f1cda3
Leon Noel <leon.a.noel+github@gmail.com>
2016-02-23 19:59:09 -0500

    dot

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
      var $gallery = $('#FlickrImages');                                            
     //Flickr Integration
-    $.getJSON("https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&api_key=c7164eb5743c0f064666cdb4856e0a11&user_id=aaronshenette&format=json", function(data){
+    $.getJSON("https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&api_key=c7164eb5743c0f064666cdb4856e0a11&user_id=aaron.shenette&format=json", function(data){
         $.each(data.items, function(i,item){
             if(i<=9999){
                 $("<img/>").attr("src", item.media.m).appendTo($gallery)
