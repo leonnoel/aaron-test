a90692f78babf2631ffb9b682cae4a282d7690de
Leon Noel <leon.a.noel+github@gmail.com>
2016-02-23 19:52:15 -0500

    https again...

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
      var $gallery = $('#FlickrImages');                                            
     //Flickr Integration
-    $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?id=aaron.shenette&lang=en-us&format=json&jsoncallback=?", function(data){
+    $.getJSON("https://api.flickr.com/services/feeds/photos_public.gne?id=aaron.shenette&lang=en-us&format=json&jsoncallback=?", function(data){
         $.each(data.items, function(i,item){
             if(i<=9999){
                 $("<img/>").attr("src", item.media.m).appendTo($gallery)
