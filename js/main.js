aed25eac031d532d939431b6c4d960dc8465a9b6
leonnoel <leon.a.noel+github@gmail.com>
2016-02-23 19:45:30 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+     var $gallery = $('#FlickrImages');                                            
+    //Flickr Integration
+    $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne", function(data){
+        $.each(data.items, function(i,item){
+            if(i<=9999){
+                $("<img/>").attr("src", item.media.m).appendTo($gallery)
+                .wrap("<a class='gallery-image' href='" + item.link + "' target='_blank' title='Flickr'></a>");
+            }
+        });
+       $gallery.imagesLoaded( function() {
+         $gallery.masonry();
+        });            
+    });                     
+});
